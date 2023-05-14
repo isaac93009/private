034bc40841f21ce186b4f909d6544b917ae3c708
isaac93009 <133547381+isaac93009@users.noreply.github.com>
2023-05-13 20:15:00 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 input.onButtonPressed(Button.AB, function () {
-    if (input.soundLevel() == 120) {
+    if (input.soundLevel() == 20) {
+        input.setSoundThreshold(SoundThreshold.Loud, 255)
         music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
     }
 })
 music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 5000, 2379, 0, 255, 4274, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
 music.setBuiltInSpeakerEnabled(true)
 basic.showIcon(IconNames.Happy)
+let sound = input.soundLevel()
 basic.forever(function () {
 	
 })
